96c0c22edf63d05470411ceb69cf6f488b09c10e
Alexander Yugay <alexander.yugay@gmail.com>
2023-08-10 19:33:06 -0400

    Car and Plane classes added

---
+++ src/Vehicle/Car.java
@@ -0,0 +1,7 @@
+package Vehicle;
+
+public class Car {
+    public static void main(String[] args) {
+        String Model;
+    }
+}
---
+++ src/Vehicle/Plain.java
@@ -0,0 +1,7 @@
+package Vehicle;
+
+public class Plain {
+    public static void main(String[] args) {
+        System.out.println("Plain flies");
+    }
+}
